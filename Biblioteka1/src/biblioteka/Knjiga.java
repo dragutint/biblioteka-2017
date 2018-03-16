5b3c81ef90d87c7ea31f7a92429ed2662b31601b
Dragutin Todorovic <Dragutin Todorovic@DESKTOP-3K036KM>
2018-03-16 20:36:52 +0100

    Dodao sam getere i setere, v 2.0

--- Biblioteka1/src/biblioteka/Knjiga.java
+++ Biblioteka1/src/biblioteka/Knjiga.java
@@ -6,6 +6,45 @@ public class Knjiga {
 	private String autori;
 	private String izdavac;
 	private int izdanje;
-	
-	
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+
+	public long getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(long iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getAutori() {
+		return autori;
+	}
+
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+
+	public String getIzdavac() {
+		return izdavac;
+	}
+
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdanje() {
+		return izdanje;
+	}
+
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	// sadaskdmas 
 }
